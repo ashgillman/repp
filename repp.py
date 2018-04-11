6dbf3b7a69ba1e567784a4518c87bd8d5c389716
Ashley Gillman <ashley.gillman@csiro.au>
2018-04-11 19:34:50 +1000

    [FIX] [REF] Wasn't replacing HOME to ~, os.path -> path

--- repp.py
+++ repp.py
@@ -1,4 +1,5 @@
 import os
+from os import path
 import subprocess
 import re
 from contextlib import contextmanager
@@ -7,7 +8,7 @@ import click
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
-DEFAULT_ROOT = os.path.expanduser('~/dev')  # TODO, be able to change
+DEFAULT_ROOT = path.expanduser('~/dev')  # TODO, be able to change
 GITHUB_RE = re.compile(
     '(?P<protocol>(?:(?:(?:http|https)://|git@))?)(github.com)(:|/)'
     '(?P<owner>\w+)(/)(?P<repo>\w+)(.git)?')
@@ -33,23 +34,26 @@ def list():
 
 
 def tree(directory, depth, _first=True):
-    lines = [directory] if _first else [os.path.basename(directory)]
-    directory = os.path.expanduser(directory)
+    lines = [pretty_path(directory)] if _first else [path.basename(directory)]
 
     paths = sorted(os.listdir(directory), key=lambda s: s.lower())
-    for idx, path in enumerate(paths):
+    for idx, inner in enumerate(paths):
         last = idx == len(paths) - 1
         outsep = '└── ' if last else '├── '
         innsep = '    ' if last else '│   '
-        pathfull = os.path.join(directory, path)
-        if os.path.isdir(pathfull) and depth > 1:
+        pathfull = path.join(directory, inner)
+        if path.isdir(pathfull) and depth > 1:
             first = True
             for line in tree(pathfull, depth-1, False).splitlines():
                 lines.append((outsep if first else innsep) + line)
                 first = False
         else:
-            lines.append(outsep + path)
+            lines.append(outsep + inner)
     return '\n'.join(lines)
+
+
+def pretty_path(p):
+    return p.replace(path.expanduser('~'), '~')
 
 
 @cli.command()
@@ -64,10 +68,10 @@ def clone(url):
 def clone_like_github(owner, repo, site='github.com'):
     clone_url = f'git@{site}:{owner}/{repo}.git'
     local_user = f'{owner}@{site}'
-    os.makedirs(os.path.join(root, local_user), exist_ok=True)
-    with pushd(os.path.join(root, local_user)):
+    os.makedirs(path.join(root, local_user), exist_ok=True)
+    with pushd(path.join(root, local_user)):
         subprocess.check_call(['git', 'clone', clone_url])
-    os.chdir(os.path.join(root, local_user, repo))
+    os.chdir(path.join(root, local_user, repo))
 
 
 @contextmanager
